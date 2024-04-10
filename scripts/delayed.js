2a5849eeaa2bb1b8922ebb81d399a6960f079115
AdobeChrisW <124885968+AdobeChrisW@users.noreply.github.com>
2024-04-10 12:04:26 +0100

    adding blank url window for link

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -9,5 +9,5 @@ let params = new URL(document.location.toString()).searchParams;
 let query = params.get("email");
 document.getElementById('email').value=query;
 
-document.getElementById("storagetypeinfo-description").innerHTML = 'Click here for details on how to get S3/Blob Storage <a href="https://adobe.ly/3OpHtLK">adobe.ly/3OpHtLK<a>';
+document.getElementById("storagetypeinfo-description").innerHTML = 'Click here for details on how to get S3/Blob Storage <a href="https://adobe.ly/3OpHtLK" target="_blank">adobe.ly/3OpHtLK<a>';
 
